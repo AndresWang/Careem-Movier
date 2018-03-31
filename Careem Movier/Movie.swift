ff450b8ed89f7e43b0366d85d3fd29232bf25eeb
Andres Wang <gibson90730047@icloud.com>
2018-03-31 12:17:48 +0200

    Movie model separated from JSON, for separation of concern

---
+++ Careem Movier/Movie.swift
@@ -0,0 +1,25 @@
+//
+//  Movie.swift
+//  Careem Movier
+//
+//  Created by Andres Wang on 2018/3/31.
+//  Copyright © 2018 Andres Wang. All rights reserved.
+//
+
+import Foundation
+
+// Note: Our own data model just in case​ ​we​ ​are​ ​asked​ ​to​ ​change​ ​to​ ​xml-based​ ​api​ ​instead​ ​of​ ​json, separation of concern.
+struct Movie {
+    struct Response {
+        var page: Int
+        var total_results: Int
+        var total_pages: Int
+        var results: [Result]
+    }
+    struct Result {
+        var title: String
+        var poster_path: String
+        var overview: String
+        var release_date: String
+    }
+}
--- Careem Movier/MoviedbAPI.swift
+++ Careem Movier/MoviedbAPI.swift
@@ -15,23 +15,32 @@ struct MoviedbAPI {
         case large = "​/w500"
         case extraLarge = "/w780"
     }
+    
+    // JSON Structure
     struct JSON {
         struct Response: Codable {
             var page: Int
             var total_results: Int
             var total_pages: Int
             var results: [Result]
+            
+            func toMovie() -> Movie.Response {
+                return Movie.Response(page: page, total_results: total_results, total_pages: total_pages, results: results.map{$0.toMovie()})
+            }
         }
-        
         struct Result: Codable {
             var title: String
             var poster_path: String
             var overview: String
             var release_date: String
+            
+            func toMovie() -> Movie.Result {
+                return Movie.Result(title: title, poster_path: poster_path, overview: overview, release_date: release_date)
+            }
         }
     }
     
-    // MARK: Helper Methods
+    // MARK: - Helper Methods
     static func searchURL(with text: String, page: Int) -> URL {
         let encodedText = text.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)!
         let searchTerm = String(format: "http://api.themoviedb.org/3/search/movie?api_key=2696829a81b1b5827d515ff121700838&query=%@&page=%ld", encodedText, page)
